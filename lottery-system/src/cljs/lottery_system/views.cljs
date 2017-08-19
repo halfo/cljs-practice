c14c19fe48c206f4970bd594aeb17194799b9e54
Hasib Billah <hasib@freightos.com>
2017-08-19 22:01:40 +0600

    Show participant list.

--- lottery-system/src/cljs/lottery_system/views.cljs
+++ lottery-system/src/cljs/lottery_system/views.cljs
@@ -1,8 +1,27 @@
 (ns lottery-system.views
   (:require [re-frame.core :as re-frame]))
 
-(defn main-panel []
-  (let [winner-id (re-frame/subscribe [:winner])
-        participants (re-frame/subscribe [:participants])]
+(defn- show-participant
+  [partiicipant]
+  [:li partiicipant])
+
+(defn- show-participants
+  [participants]
+  [:div
+    [:h3 "Participant List:"]
+    [:ul (map show-participant participants)]])
+
+(defn- show-winner
+  [participants winner-id]
+  (if (= winner-id nil)
+    nil
+    [:div "Winner is " (get participants winner-id)]))
+
+(defn main-panel
+  []
+  (let [winner-id @(re-frame/subscribe [:winner])
+        participants @(re-frame/subscribe [:participants])]
     (fn []
-      [:div "Winner is " (get @participants @winner-id)])))
+      [:div
+        (show-winner participants winner-id)
+        (show-participants participants)])))
